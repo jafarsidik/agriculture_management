6beed8248e73b9bb42eddea605a1bc08fab27e7e
jafarsidik <jeff.sidik@gmail.com>
2026-05-18 16:07:10 +0700

    bugs fix

--- agriculture_management/agriculture_management/tasks.py
+++ agriculture_management/agriculture_management/tasks.py
@@ -27,11 +27,11 @@ def check_maintenance_due():
 		
     equipment_list = frappe.get_all(		
         "Farm Equipment",		
-        filters={		
-            "status":           ["!=", "Retired"],		
-            "next_maintenance": ["<=", threshold],		
-            "next_maintenance": ["!=", None],		
-        },		
+        filters=[
+            ["status", "!=", "Retired"],
+            ["next_maintenance", "<=", threshold],
+            ["next_maintenance", "is", "set"],
+        ]
         fields=["name", "equipment_name", "next_maintenance", "assigned_farm"]		
     )		
 		
