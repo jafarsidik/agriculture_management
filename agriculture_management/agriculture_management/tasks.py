7c904334e4ea309d63eb9546fc43bbf0d3db90db
jafarsidik <jeff.sidik@gmail.com>
2026-05-18 16:17:41 +0700

    bugs task

--- agriculture_management/agriculture_management/tasks.py
+++ agriculture_management/agriculture_management/tasks.py
@@ -12,7 +12,6 @@
 #        ]		
 #    }		
 # ============================================================		
-
 import frappe		
 from frappe import _		
 from frappe.utils import today, add_days, getdate		
@@ -26,15 +25,15 @@ def check_maintenance_due():
     """		
     threshold = add_days(today(), 14)		
 		
-    equipment_list = frappe.get_all(		
-        "Farm Equipment",		
+    equipment_list = frappe.get_all(
+        "Farm Equipment",
         filters=[
             ["status", "!=", "Retired"],
             ["next_maintenance", "<=", threshold],
             ["next_maintenance", "is", "set"],
-        ]
-        fields=["name", "equipment_name", "next_maintenance", "assigned_farm"]		
-    )		
+        ],
+        fields=["name", "equipment_name", "next_maintenance", "assigned_farm"]
+    )	
 		
     for eq in equipment_list:		
         # Skip if a scheduled/draft maintenance already exists		
